e9ec1ab19c7d1235e2b3c6d0eda334c10c2b52e4
Jean <chakib3500@gmail.com>
2022-03-23 21:40:46 +0100

    header entierement fonctionel

---
+++ js/header.js
@@ -0,0 +1,56 @@
+//---------------hamburger-btn & mobile-menu-------------------------------
+let CSSbreakPoint = 935;
+
+
+let hamburger = document.querySelector(".hamburger");
+let mombileMenu = document.querySelector(".mobile-header-menu");
+
+    hamburger.addEventListener("click",()=>{
+        if(hamburger.classList.contains("active"))
+        {
+            
+            hamburger.classList.add("inactive");
+            mombileMenu.classList.add("inactive");
+
+
+            setTimeout(() => {
+                hamburger.classList.remove("active");
+                mombileMenu.classList.remove("active");                
+            }, 100);
+        }
+        else
+        {
+            hamburger.classList.add("active");
+            mombileMenu.classList.remove("inactive");
+            mombileMenu.classList.add("active");
+            setTimeout(() => {
+                hamburger.classList.remove("inactive");
+            }, 100);
+        }
+    })
+
+    window.addEventListener("resize",()=>{
+        if (window.innerWidth>=CSSbreakPoint) {
+            mombileMenu.classList.remove("inactive");
+            mombileMenu.classList.remove("active");
+            hamburger.classList.remove("active");
+            hamburger.classList.remove("inactive");
+        }
+    })
+
+//-----------------header menu---------------------------
+
+let docs = document.querySelector(".navbar > li");
+    let headerMenu = document.querySelector(".header-menu");
+    
+    docs.addEventListener("mouseover",()=>{
+        headerMenu.classList.add("active");
+    })
+    
+    headerMenu.addEventListener("mouseleave",()=>{
+        headerMenu.classList.add("inactive");
+        setTimeout(() => {
+            headerMenu.classList.remove("active");
+            headerMenu.classList.remove("inactive");
+        }, 300);
+    })
